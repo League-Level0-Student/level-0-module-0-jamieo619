1a716f5d4f16db8b723fd0333f6b27164e6a197c
307304 <307304@0338S-Z-90GSYPV.sdcs.local>
2018-04-03 14:27:21 -0700

    Robot

---
+++ src/RobotGraffiti.java
@@ -0,0 +1,30 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class RobotGraffiti {
+
+public static void main(String[] args) {
+	Robot juan = new Robot();
+	juan.miniaturize();
+	juan.penDown();
+	juan.sparkle();
+	juan.setPenWidth(5);
+	juan.setSpeed(20);
+	juan.move(180);
+	juan.turn(90);
+	juan.move(100);
+	juan.turn(180);
+	juan.move(200);
+	juan.turn(180);
+	juan.move(100);
+	juan.turn(90);
+	juan.move(180);
+	juan.turn(90);
+	juan.move(100);
+	juan.turn(90);
+	juan.move(90);
+	
+}
+
+
+}
+
--- src/RobotSquare.java
+++ src/RobotSquare.java
@@ -11,22 +11,25 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
+    	Robot juan = new Robot();
+    	juan.miniaturize();
 
         // 3. Put the robot's pen down
-
+    	juan.penDown();
 
         // 6. Make the robot move as fast as possible
-
+    	juan.setSpeed(10);
 
         // 5. Do everything below here 4 times
-
-
+    	for(int i=0;i<4;i++) {
+    		juan.move(200);
+    		juan.turn(90);
+    	}	
         //         2. Move your robot 200 pixels
-
+    	//juan.move(200);
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
-
+    	//juan.turn(90);
 
     }
 }
